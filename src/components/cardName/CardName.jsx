1b0ca909a4831917c8375635a3bdd6384ea103c3
Мой Господин <erjsuy@gmail.com>
2025-04-10 13:14:01 +0600

    change img

--- src/components/cardName/CardName.jsx
+++ src/components/cardName/CardName.jsx
@@ -3,21 +3,27 @@ import Mas from '../img/Mas.png'
 import air from '../img/air.png'
 import Uber from '../img/uber.png'
 import Pay from '../img/pay.png'
-import Visa from '../img/Visa.png'
-import Stripe from '../img/Stripe.png'
+import Visa from '../img/visa.png'
+import Stripe from '../img/stripe.png'
 
 const CardName = () => {
   return (
     <>
-    <div className='bg-red-400  w-full h-66 flex justify-center items-center'>
-      <p className='text-3xl' >Trusted by 20,000+ companies</p>
-      <img src={Mas} alt="" />
-      <img src={air} alt="" />
-      <img src={Uber} alt="" />
-      <img src={Pay} alt="" />
-      <img src={Visa} alt="" />
-      <img src={Stripe} alt="" />
-    </div>
+    <div className="">
+        <div className="w-8/12 h-66 m-auto mt-10 ">
+          <p className="text-[#244D4D] flex items-center justify-center text-4xl font-bold">
+            Trusted by 20,000+ companies
+          </p>
+          <div className="flex justify-between items-center  mt-20">
+            <img src={Mas} alt="" />
+            <img src={air} alt="" />
+            <img src={Uber} alt="" />
+            <img src={Pay} alt="" />
+            <img src={Visa} alt="" />
+            <img src={Stripe} alt="" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
